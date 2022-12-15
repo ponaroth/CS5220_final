0f7917b4962683780eab52814063b874817067cf
Ponaroth Eab <ronadean@hotmail.com>
2022-12-15 12:27:08 -0800

    add git ignore

--- express-quiz-app/server.js
+++ express-quiz-app/server.js
@@ -5,14 +5,11 @@ const port = 8888;
 const quizzes = require('./api/routes/quizzes');
 
 app.use(cors());
-
-// middleware to parse POST/PUT bodies in express
 app.use(express.json());
 
-// add resource route to our express application
+// add route
 app.use('/quizzes', quizzes);
 
-// start the server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
