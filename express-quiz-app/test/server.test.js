d276a690c1976d7d8c14573bd2688142f8abc77c
Ponaroth Eab <ronadean@hotmail.com>
2022-12-15 20:52:05 -0800

    add methods

--- express-quiz-app/test/server.test.js
+++ express-quiz-app/test/server.test.js
@@ -1,59 +1,129 @@
-const supertest = require('supertest');
+const request = require('supertest');
 const server = require('../server');
 const base64 = require('base-64');
 
-describe('1. GET quizzes with expectation tests', function() {
-test("GET all quizzes", async () => {
-	await supertest(server)
-		.get("/quizzes")
-		.expect(200)
-		.then((response) => {
-			// Check the response type and length
-			expect(Array.isArray(response.body)).toBeTruthy()
-			expect(response.body.length).toEqual(2)
-			expect(response.body).toBeInstanceOf(Array)
-			// Check the response data
-			expect(response.body[0].id).toBe(1)
-			expect(response.body[0]).toHaveProperty('id');
-			expect(response.body[0]).toHaveProperty('name');
-		})
-});
+describe('1. GET quizzes with expectation tests', function () {
+  test("GET all quizzes", async () => {
+    await request(server)
+      .get("/quizzes")
+      .expect(200)
+      .then((response) => {
+        // Check the response type and length
+        expect(Array.isArray(response.body)).toBeTruthy()
+        expect(response.body.length).toEqual(2)
+        expect(response.body).toBeInstanceOf(Array)
+        // Check the response data
+        expect(response.body[0].id).toBe(1)
+        expect(response.body[0]).toHaveProperty('id');
+        expect(response.body[0]).toHaveProperty('name');
+      })
+  });
 
-test('GET one quiz by id test',async () => {
-	await supertest(server)
-	  .get('/quizzes/1')
-	  .expect(200)
-	  .expect((res) => {
-		expect(typeof res.body.id).toBe('number');
-		expect(typeof res.body.name).toBe('string');
-		expect(typeof res.body.instructions).toBe('string');
-		expect(typeof res.body.start).toBe('string');
-		expect(typeof res.body.end).toBe('string');
-		expect(typeof res.body.timer).toBe('object');
-		expect(typeof res.body.timer.isTimed).toBe('boolean');
-		expect(typeof res.body.timer.time_limit).toBe('number');
-		expect(Array.isArray(res.body.question_ids)).toBe(true);
-	  });
+  test('GET one quiz by id test', async () => {
+    await request(server)
+      .get('/quizzes/1')
+      .expect(200)
+      .expect((res) => {
+        expect(typeof res.body.id).toBe('number');
+        expect(typeof res.body.name).toBe('string');
+        expect(typeof res.body.instructions).toBe('string');
+        expect(typeof res.body.start).toBe('string');
+        expect(typeof res.body.end).toBe('string');
+        expect(typeof res.body.timer).toBe('object');
+        expect(typeof res.body.timer.isTimed).toBe('boolean');
+        expect(typeof res.body.timer.time_limit).toBe('number');
+        expect(Array.isArray(res.body.question_ids)).toBe(true);
+      });
+  });
+
+  test('Add quiz test', async () => {
+    let newQuiz = {
+      "name": "Quiz 3",
+      "instructions": "test text.",
+      "timer": {
+          "isTimed": true,
+          "time_limit": 10
+      },
+      "question_ids": []
+  }
+
+    await request(server)
+      .post('/quizzes')
+      .send(newQuiz)
+      .expect(200)
+      .expect((res) => {
+        res.body = newQuiz;
+      });
   });
 });
 
+//// Add and Update Request ////
+describe("2. Add and Update Quiz test", function () {
+  test('Add quiz should succeed', async () => {
+    let newQuiz = {
+      "name": "Quiz 3",
+      "instructions": "test text.",
+      "timer": {
+          "isTimed": true,
+          "time_limit": 10
+      },
+      "question_ids": []
+  }
+    await request(server)
+      .post('/quizzes')
+      .send(newQuiz)
+      .expect(200)
+      .expect((res) => {
+        res.body = newQuiz;
+      });
+  });
 
-///// mocha describe() and authorization with token ////
-describe('2. GET /answers with bearer token', function() {
-  it('responds with json', function(done) {
-    supertest(server)
+  test('Update quiz should succeed', async () => {
+    let update = {
+      "question_ids": [4, 5]
+  }
+    await request(server)
+      .put('/quizzes/1')
+      .send(update)
+      .expect(200)
+      .expect((res) => {
+        res.body.question_ids = update.question_ids;
+      });
+  });
+});
+
+//// Delete and Error test ////
+describe("3. Delete and Error test", function () {
+  it('Delete quiz 1 should succeed', function (done) {
+    request(server)
+      .delete('/quizzes/1')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+
+  it('Delete quiz 4 should throw 404 error', function (done) {
+    request(server)
+      .delete('/quizzes/4')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
+});
+
+//// Authorization ////
+describe("4. Authorization test", function () {
+  it('GET /answers with bearer token', function (done) {
+    request(server)
       .get('/answers/byToken')
       .set('Authorization', 'Bearer 12345')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
-});
 
-///// mocha describe() and authorization with username and password ////
-describe('3. GET /answers with username and password', function() {
-  it('responds with json', function(done) {
-    supertest(server)
+  it('GET /answers with username and password', function (done) {
+    request(server)
       .get('/answers/byCredentials')
       .set('Authorization', 'Basic ' + base64.encode('admin:password'))
       .set('Accept', 'application/json')
