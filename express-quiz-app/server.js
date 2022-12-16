f6b20ea07a7bbfc74b2263a3443f645ce097251f
Ponaroth Eab <ronadean@hotmail.com>
2022-12-15 18:19:39 -0800

    add answers

--- express-quiz-app/server.js
+++ express-quiz-app/server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const port = 8888;
 const quizzes = require('./api/routes/quizzes');
+const answers = require('./api/routes/AnswerRouter');
 
 app.use(cors());
 app.use(express.json());
 
 // add route
 app.use('/quizzes', quizzes);
+app.use('/answers', answers);
 
 const server = app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
--- express-quiz-app/test/server.test.js
+++ express-quiz-app/test/server.test.js
@@ -1,7 +1,8 @@
 const supertest = require('supertest');
 const server = require('../server');
+const base64 = require('base-64');
 
-test("GET /quizzes", async () => {
+test("1. Get quiz ", async () => {
 	await supertest(server)
 		.get("/quizzes")
 		.expect(200)
@@ -14,4 +15,30 @@ test("GET /quizzes", async () => {
 			expect(response.body[0].id).toBe(1)
 
 		})
-})
+});
+
+
+///// mocha describe() and authorization with token ////
+describe('2. GET /answers with bearer token', function() {
+  it('responds with json', function(done) {
+    supertest(server)
+      .get('/answers/byToken')
+      .set('Authorization', 'Bearer 12345')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
+
+///// mocha describe() and authorization with username and password ////
+describe('2. GET /answers with username and password', function() {
+
+  it('responds with json', function(done) {
+    supertest(server)
+      .get('/answers/byCredentials')
+      .set('Authorization', 'Basic ' + base64.encode('admin:password'))
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
