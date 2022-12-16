a31856ea482946d8e0ba0feda737dfeb4031fa29
Ponaroth Eab <ronadean@hotmail.com>
2022-12-15 19:30:43 -0800

    add Eman's stuff

--- express-quiz-app/test/server.test.js
+++ express-quiz-app/test/server.test.js
@@ -2,7 +2,8 @@ const supertest = require('supertest');
 const server = require('../server');
 const base64 = require('base-64');
 
-test("1. Get quiz ", async () => {
+describe('1. GET quizzes with expectation tests', function() {
+test("GET all quizzes", async () => {
 	await supertest(server)
 		.get("/quizzes")
 		.expect(200)
@@ -10,11 +11,30 @@ test("1. Get quiz ", async () => {
 			// Check the response type and length
 			expect(Array.isArray(response.body)).toBeTruthy()
 			expect(response.body.length).toEqual(2)
-
+			expect(response.body).toBeInstanceOf(Array)
 			// Check the response data
 			expect(response.body[0].id).toBe(1)
-
+			expect(response.body[0]).toHaveProperty('id');
+			expect(response.body[0]).toHaveProperty('name');
 		})
+});
+
+test('GET one quiz by id test',async () => {
+	await supertest(server)
+	  .get('/quizzes/1')
+	  .expect(200)
+	  .expect((res) => {
+		expect(typeof res.body.id).toBe('number');
+		expect(typeof res.body.name).toBe('string');
+		expect(typeof res.body.instructions).toBe('string');
+		expect(typeof res.body.start).toBe('string');
+		expect(typeof res.body.end).toBe('string');
+		expect(typeof res.body.timer).toBe('object');
+		expect(typeof res.body.timer.isTimed).toBe('boolean');
+		expect(typeof res.body.timer.time_limit).toBe('number');
+		expect(Array.isArray(res.body.question_ids)).toBe(true);
+	  });
+  });
 });
 
 
@@ -31,8 +51,7 @@ describe('2. GET /answers with bearer token', function() {
 });
 
 ///// mocha describe() and authorization with username and password ////
-describe('2. GET /answers with username and password', function() {
-
+describe('3. GET /answers with username and password', function() {
   it('responds with json', function(done) {
     supertest(server)
       .get('/answers/byCredentials')
@@ -42,3 +61,6 @@ describe('2. GET /answers with username and password', function() {
       .expect(200, done);
   });
 });
+
+
+server.close();
